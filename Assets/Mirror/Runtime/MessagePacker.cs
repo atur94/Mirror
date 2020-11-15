e21003924ea0cf282a8d94eb42cfebbe3355f273
vis2k <info@noobtuts.com>
2020-11-16 02:51:34 +0800

    Update Assets/Mirror/Runtime/MessagePacker.cs

--- Assets/Mirror/Runtime/MessagePacker.cs
+++ Assets/Mirror/Runtime/MessagePacker.cs
@@ -103,6 +103,7 @@ namespace Mirror
                 NetworkDiagnostics.OnReceive(message, channelId, reader.Length);
             }
 
+            // user handler exception should not stop the whole server
             try
             {
                 // user implemented hanlder
